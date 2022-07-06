5e4fcd5a0959e442229ceb12706c4c69f05fa299
Kerotobil <84286874+Kerotobil@users.noreply.github.com>
2022-07-06 14:57:26 +0300

    Add product card

--- components/cardProduct.jsx
+++ components/cardProduct.jsx
@@ -3,28 +3,26 @@ import Link from 'next/link';
 
 export const CardProduct = (props) => {
   return (
-    <div>
-      <Link href={`/book/${props.id}`} passHref>
-        <div className="card-product rounded shadow">
-          <div className="card-product-image">
-            <Image
-              src={props.image}
-              priority
-              layout="fill"
-              objectFit="contain"
-              className="bg-secondary py-1"
-              alt={props.name}
-            />
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">{props.name}</h5>
-              <p className="card-text card-product-description">{props.author}</p>
-            </div>
-            <div className="card-product-info text-center rounded-lg mx-auto mb-2">{props.price + ' ₺'}</div>
-          </div>
+    <Link href={`/book/${props.id}`} passHref>
+      <div className="card-product rounded shadow">
+        <div className="card-product-image">
+          <Image
+            src={props.image}
+            priority
+            layout="fill"
+            objectFit="contain"
+            className="bg-secondary py-1"
+            alt={props.name}
+          />
         </div>
-      </Link>
-    </div>
+        <div className="card">
+          <div className="card-body">
+            <p className="card-title">{props.name}</p>
+            <p className="card-text card-product-description">{props.author}</p>
+          </div>
+          <div className="card-product-info text-center rounded-lg mx-auto mb-2">{props.price + ' ₺'}</div>
+        </div>
+      </div>
+    </Link>
   );
 };
